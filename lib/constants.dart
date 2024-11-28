3e5c8b0ea18ef0c5b69f1436c802d4e308c73393
Felipe Cesar <fel.cesar@gmail.com>
2024-11-28 17:54:09 -0300

    feat: :lipstick: Design opening page

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class Constants {
+  // Text
+  static const smartForms = 'Smart Forms';
+  static const createNewForm = 'Create New Form';
+
+  // Colors
+  static const blueHighlight = Color(0xFF2462D1);
+  static const gray200 = Color(0xFFEDF2F7);
+  static const grey800 = Color(0xFF2D3748);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_forms/constants.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,66 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Constants.gray200,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Constants.blueHighlight,
+            foregroundColor: Colors.white,
+          ),
+        ),
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('<LOGO GOES HERE>'),
+                    SizedBox(height: 20),
+                    Text(
+                      Constants.smartForms,
+                      style: TextStyle(
+                        color: Constants.grey800,
+                        fontSize: 42,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(48),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(35),
+                ),
+                color: Colors.white,
+              ),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Column(
+                  children: [
+                    SizedBox(height: 20),
+                    Text(
+                      Constants.createNewForm,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
